e4fd549fc4e6cda775cbbd0db553a6fd3414d39f
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-10-19 14:59:45 +0200

    docker compose ...

--- uninstall-Caddy.sh
+++ uninstall-Caddy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #Created by Manfred 19.10.2025
 
-compose -f /opt/caddy/compose.yaml down
-compose -f /opt/caddy/compose-http-server.yaml down
+docker compose -f /opt/caddy/compose.yaml down
+docker compose -f /opt/caddy/compose-http-server.yaml down
 docker image rm caddy:latest
 docker network rm caddy_net
 sudo rm -rfv /opt/caddy
