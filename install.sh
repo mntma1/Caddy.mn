32ff4dfad329b01fbba168c3c311d97f9508d265
Manfred N <114916491+mntma1@users.noreply.github.com>
2026-04-08 16:28:33 +0200

    Kleine Änderungen

--- install.sh
+++ install.sh
@@ -3,16 +3,20 @@
 # Date: 23.08.2025
 
 CFILE=/opt/caddy/Caddyfile
+
+# Fragt deine E-MAil Adresse ab.
 clear
-echo "Eine E-Mail Adresse für Let's Encrypt"
-echo "===================================="
+echo "Trage hier eine gültige E-Mail Adresse für Let's Encrypt hinzu"
+echo "======================================================"
 read -p 'Die E-Mail Adresse bitte: ' EMAIL
+
 # Legt die Verzeichnisse an und kopiert die Dateien (überschreibt vorhandene).
 sudo mkdir -pv /opt/caddy/{data,config,file}
 sudo chown -Rv $USER: /opt/caddy
 cp -fv install.sh compose.yaml compose-http-server.yaml /opt/caddy/  
 cp -fv index.html /opt/caddy/file
 
+# Trägt deine E-Mail Adresse ins Caddyfile ein.
 cat<<editcaddyfile>$CFILE
 # eMail für Let's encrypt
 {
@@ -20,9 +24,14 @@ cat<<editcaddyfile>$CFILE
 }
 editcaddyfile
 
+# Startet Caddy
 docker network create caddy_net
 docker compose -f /opt/caddy/compose.yaml up -d
 docker compose -f /opt/caddy/compose-http-server.yaml up -d
+clear;
+echo ...warte 3 Sekunden;
+sleep 3; 
+docker stats --no-stream
 echo
 
 
@@ -34,5 +43,8 @@ Die E-Mail Adresse für Let's Encrypt lautet: $EMAIL
 Der simple-http-server ist zu erreichen unter: http://$(hostname -I | awk '{print $1}' | cut -d/ -f1):81
 
 => Füge nun eine Domain mit: -> ./addDomain.sh <- hinzu
+
+....Das war einfach :-)
+
 info
 exit 0
